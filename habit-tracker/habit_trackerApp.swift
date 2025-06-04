4ea8508c11e19f9a3d495e06a25515484e433ba7
Liv Motley <livmotley.uk@gmail.com>
2025-06-04 13:59:31 +0100

    Initial Commit

---
+++ habit-tracker/habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_trackerApp.swift
+//  habit-tracker
+//
+//  Created by Liv Motley on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
